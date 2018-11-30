6a72a1a2d8e6b4dae962453109579c37115cb784
tophermurphy <tophermurphy@gmail.com>
2018-11-30 09:06:12 -0600

    "haven't commited in a while"

--- src/data/lgb-cw.js
+++ src/data/lgb-cw.js
@@ -41,17 +41,71 @@ module.exports = {
             ],
         content: {
             section1: {
-                header:   "Making a Useful Tool Creates It's Own Opportunities",
+                header:   "Making a Useful Tool Creates<br class='lg-break'/>  It's Own Opportunities",
                 body: [
                     "The goal was to create a website that becomes a useful tool to sales reps and accounts. Making the right beer easier to find will promote sales and increase traffic leading to other marketing opportunities. The site needs to reflect the companies history and expertise while looking forward and relevant to the market today."
                     ],
                 images: [
+                    {
+                        filename: "glunzbeers-home-desktop.jpg",
+                        alt: "Desktop home page for glunzbeers.com"
+                    },
+                    {
+                        filename: "glunzbeers-home-mobile.png",
+                        alt: "Mobile home page for glunzbeers.com"
+                    },
+                    {
+                        filename: "glunzbeers-home-tablet.png",
+                        alt: "Tablet home page for glunzbeers.com"
+                    }
                 ]
             },
             section2: {
                 header:   "Goals and Personas Are a Framework for Decision Making",
                 body: [
                     "To establish a framework to guide the process goals were stated, personas created and a survey made. After the primary inquiry the initial site architecture and wireframing was done. Secondary feedback from the sales staff lead to insights that local and new beers were the most prominent categories accounts were searching for."
+                ],
+                images: [
+                    {
+                        filename: "glunzbeers-wireframe-0.png",
+                        alt: "Desktop home page wireframe for glunzbeers.com"
+                    },
+                    {
+                        filename: "glunzbeers-wireframe-1.png",
+                        alt: "Desktop product search page wireframe for glunzbeers.com"
+                    },
+                    {
+                        filename: "glunzbeers-wireframe-2.png",
+                        alt: "Desktop product details page wireframe for glunzbeers.com"
+                    },
+                    {
+                        filename: "glunzbeers-wireframe-3.png",
+                        alt: "Mobile search filters wireframe for glunzbeers.com"
+                    },
+                    {
+                        filename: "glunzbeers-wireframe-4.png",
+                        alt: "Product search cards wireframe for glunzbeers.com"
+                    },
+                    {
+                        filename: "glunzbeers-wireframe-5.png",
+                        alt: "Product search cards wireframe for glunzbeers.com"
+                    },
+                    {
+                        filename: "glunzbeers-wireframe-6.png",
+                        alt: "Wireframe notes for glunzbeers.com"
+                    },
+                    {
+                        filename: "glunzbeers-wireframe-7.png",
+                        alt: "Mobile product details page wireframe for glunzbeers.com"
+                    },
+                    {
+                        filename: "glunzbeers-wireframe-8.png",
+                        alt: "Product search page wireframe for glunzbeers.com"
+                    },
+                    {
+                        filename: "glunzbeers-wireframe-9.png",
+                        alt: "Brewery details tablet page wireframe for glunzbeers.com"
+                    }
                 ]
             },
             section3: {
@@ -59,6 +113,24 @@ module.exports = {
                 body: [
                     "The path to finding the right product for the account can take different approaches. Searching by brewery or style, segment or location, or just casually browsing the latest beers. The search functionality was aided with a faceted filter allowing the user to drill down by style, season, package or abv.",
                     "On the product details page all the pieces come together. All the relevant info for the beer is here, including an image carrousel, package formats and item numbers, featured ingredients, months available and ratings from Untappd and RateBeer that trigger above a certain threshold."
+                ],
+                images: [
+                    {
+                        filename: "glunzbeers-craft-contents-page.jpg",
+                        alt: "Index of American craft breweries for glunzbeers.com"
+                    },
+                    {
+                        filename: "glunzbeers-brewery-segments-page.jpg",
+                        alt: "Brewery segments page for glunzbeers.com"
+                    },
+                    {
+                        filename: "glunzbeers-product-search-page.jpg",
+                        alt: "Product search page for glunzbeers.com"
+                    },
+                    {
+                        filename: "glunzbeers-product-details-page.jpg",
+                        alt: "Product details page for glunzbeers.com"
+                    }
                 ]
             },
             section4: {
@@ -66,12 +138,32 @@ module.exports = {
                 body: [
                     "Louis Glunz Beer Inc. has been in business since 1888 and has been a part of and grown with the beer scene in Chicago ever since. Developing the branding for the site looks to include nods to the past, but must look relevant to contemporary audiences. The previous branding only amounted to a logo. So theme, color and typography had to be established.",
                     "The strong linear drafted look is echoed by the cards styling for discrete information packets. The colors scheme are meant to evoke the colors of beer as well as tipping the hat to an earlier time. Incorporating these into gradients allows me to use a design trend and further evoke the appearance of a light or dark beer in a glass."
+                ],
+                images: [
+                     {
+                    filename: "glunzbeers-what-we-offer-page.jpg",
+                    alt: "A landing page dedicated to telling what specifically Louis Glunz Beer Inc. has to offer accounts, suppliers and consumers."
+                },
+                {
+                    filename: "glunzbeers-who-we-are-page.jpg",
+                    alt: "A landing page that tells the story of Louis Glunz Beer Inc."
+                }
                 ]
             },
             section5: {
                 header:   "Let CSS Cascade & SASS Extend",
                 body: [
                     "I used the products details page, beer post archive pages, and a style guide to construct my default styling and create reusable CSS classes. After having this code rock solid, using cascading styles and the extend feature of SASS let me put together all the supporting pages and content quickly and uniformly."
+                ],
+                images: [
+                    {
+                        filename: "category-header.svg",
+                        alt: "category header"
+                    },
+                    {
+                        filename: "glunz-typography.svg",
+                        alt: "Typographic styles for glunzbeers.com"
+                    }
                 ]
             },
             section6: {
@@ -83,6 +175,9 @@ module.exports = {
                 ],
                 body: [
                     "In the year and a half since the new site has been launched there has been a 32% increase in users and a 37% increase in sessions. As well a more prominent contact form leads to contacts from new accounts and beer drinkers.  The site has also lead to increased visibility for smaller brands. People contact us all over the world looking asking where these brands can be purchased."
+                ],
+                svgs: [
+                    '<svg version="1.1" id="up-arrow-svg" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"width="30px" height="42px" viewBox="0 0 30 42" enable-background="new 0 0 30 42" xml:space="preserve"><title>Up</title><path id="up-arrow-svg-path" d="M20.551,29.557V42H9.449V29.557H0.222L15,0l14.778,29.557H20.551z"/></svg>'
                 ]
             }
         }
