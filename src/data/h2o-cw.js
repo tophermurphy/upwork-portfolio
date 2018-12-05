12072fa35f9577219e543a537fff572b6c9219d5
tophermurphy <tophermurphy@gmail.com>
2018-12-05 11:28:50 -0600

    'another commit'

--- src/data/h2o-cw.js
+++ src/data/h2o-cw.js
@@ -2,7 +2,7 @@ module.exports = {
     h2oplussite: {
             title:    "H2O Plus",
             id:       "h2o-cw",
-            slug:     "h2o-plus-ecomm-website",
+            slug:     "h2o-plus-e-commerce-website",
             type:     "page",
             subtitle: "e-commerce site",
             description:   "Continuously evolving a brand to stay relevant and raising the quality to compete with prestige brands",
@@ -39,13 +39,42 @@ module.exports = {
                 body: [
                     "For eight years as the in-house web designer at H2O Plus I grew along with the brand. As design trends, marketing techniques and technologies grew, as well as changes in the beauty market I grew and evolved with them, always raising bar and embracing new challenges every day.",
                     "The latest site visual update. The intention was to lighten the visual feel of the site. After the last rebranding the focus was on deep sea ingredients and water. This was too heavy for the water. Also the previous website template relied to much on keeping all content above the fold."
+                    ],
+                images: [
+                    {
+                        filename: "h20-plus-classic-homepage-0.jpg",
+                        alt: "H2O Plus earliest homepage from the 90's"
+                    },
+                    {
+                        filename: "h20-plus-classic-homepage-1.jpg",
+                        alt: "H2O Plus classic homepage, with a lighter cleaner feel."
+                    },
+                    {
+                        filename: "h20-plus-classic-homepage-2.jpg",
+                        alt: "H2O Plus transistional homepage with a deep undersea background."
+                    },
+                    {
+                        filename: "h20-plus-classic-homepage-3.jpg",
+                        alt: "H2O Plus rebranding homepage with a boxless look and a deep water-like background."
+                    },
+                    {
+                        filename: "h20-plus-classic-homepage-4.jpg",
+                        alt: "H2O Plus rebranding homepage cleaning up the previous homepage and creating a fresh clinical look."
+                    }
                     ]
+                    
             },
             section2: {
                 header:   "Bigger, Brighter & Clearer",
                 body: [
                     "The homepage focused several large dynamic visuals, bold typography and quick copy to introduce the latest promotion. Although the current platform was not responsive, I was able to make the homepage adaptive, stretching the key visuals to feel the page maximizing the impage.",
                     "In addition to cleaning up the header, a larger footer was incorporated in two parts. The first to additional offers such as free shipping and more extensive site navigation at the bottom."
+                ],
+                images: [
+                    {
+                        filename: "h2o-plus-homepage.jpg",
+                        alt: "H2O Plus homepage. The fruition of a brand repositioning, for a clean, clinical look."
+                    },
                 ]
             },
             section3: {
@@ -53,6 +82,20 @@ module.exports = {
                 body: [
                     "Changing the background to white necessitated codifying a color scheme as well establishing a typographic system with the already chose typeface Trade Gothic Condensed.",
                     "Adding a light cerulean blue and soft greys allowed for the effect of a lighter cleaners water visual. <Light, Clean, Water, Science>. Partial boarders and complementing verticle and horizontal lines instead of full border helped define discrete spaces yet keep an open look."
+                ],
+                images: [
+                    {
+                        filename: "h2o-plus-typography-headers.svg",
+                        alt: "H2O Plus header typography - with Trade Gothic Condensed."
+                    },
+                    {
+                        filename: "h2o-plus-typography-paragraph.svg",
+                        alt: "H2O Plus paragraph typography — with Trade Gothic Condensed."
+                    },
+                    {
+                        filename: "h20-plus-gateway.jpg",
+                        alt: "H2O Plus gateway page featuring the different skin care collections."
+                    }
                 ]
             },
             section4: {
@@ -60,12 +103,36 @@ module.exports = {
                 body: [
                     "We did a complete overhaul of the product details page. The goal was to information dense, while  remaining easy to read and still feel light.",
                     "A well established hierarchy was necessary for a road map for the page layout. Vital concise info was present above the fold. Horizontal and vertical lines where used to define separate spaces while keeping everything open. And establishing a tight typographic scale for consistency and rhythm. "
+                ],
+                images: [
+                    {
+                        filename: "h2o-plus-product-details.jpg",
+                        alt: "H2O Plus product details page, featuring a product description, suggested products above the fold, key elements and full ingredients, videos, media appearances and reviews."
+                    }
                 ]
             },
             section5: {
                 header: "Telling the Story of the Brand in Many Different Ways",
                 body: [
                     "There were many opportunities to tell the story of our brand with the use of landing pages throughout the site. This was always a great opportunity to explore unique design options."
+                ],
+                images: [
+                    {
+                        filename: "h20-plus-landing-page-0.jpg",
+                        alt: "H2O Plus landing page featuring the skin care collections."
+                    },
+                    {
+                        filename: "h20-plus-landing-page-1.jpg",
+                        alt: "H2O Plus landing page showing the transition to the rebranded packaging."
+                    },
+                    {
+                        filename: "h20-plus-landing-page-2.jpg",
+                        alt: "H2O Plus landing page featuring the company's many philanthropic endeavors."
+                    },
+                    {
+                        filename: "h20-plus-landing-page-3.jpg",
+                        alt: "H2O Plus landing page selling the benefits of the customer loyalty program."
+                    }
                 ]
             },
             section6: {
@@ -73,11 +140,47 @@ module.exports = {
                 body: [
                     "The first big impact I was able to make on h2oplus.com was to create an implement a new stanadard for product photography. Consistent high quality product photography was primary in communicating that we were a prestige brand. I undertook rephotographing the entire collection to bring all products on the site up to this standard.",
                     "Over the years I was able to create colorful high impact story telling photos that anchored marketing promotions and brought the products to life in a new an fun way."
+                ],
+                images: [
+                    {
+                        filename: "h2o-photography-product-silhouettes.jpg",
+                        alt: "A collection of different product silhouette photos."
+                    },
+                    {
+                        filename: "h2o-plus-photography-serum-splash.jpg",
+                        alt: "A styled photo of serum in a rippling pool of water."
+                    },
+                    {
+                        filename: "h2o-plus-photography-whitening.jpg",
+                        alt: "A styled photo of whitening serum on a bright and bubbly background."
+                    },
+                    {
+                        filename: "h2o-plus-photography-lip-candy.jpg",
+                        alt: "A styled photo of lip balm on a background of candy hearts."
+                    },
+                    {
+                        filename: "h2o-plus-photography-night-treatment.jpg",
+                        alt: "A styled photo of a night treatment gel on a starry night background with rippling water."
+                    },
+                    {
+                        filename: "h2o-plus-photography-holiday-sparkles.jpg",
+                        alt: "A styled photo of a skin care collection on a background of festive red holiday sparkles."
+                    },
+                    {
+                        filename: "h2o-plus-photography-bath-bubbles.jpg",
+                        alt: "A styled photo of a pair of bath washes amidst floating bubbles."
+                    }
                 ]
             },
             section7: {
                 body: [
                     "Over the years at H2O Plus I was able to continually create dymanic imagery, relevant and stunning web sites that remained relevant elevating the brand to compete with larger prestige brands. The site was recognized as MarketLive Site on the Rise in 2011. The quality of work helped gain venture capital and foreign investment."
+                ],
+                images: [
+                    {
+                        filename: "h2o-plus-website-award.jpg",
+                        alt: "H2O Plus award from MarketLive for website on the rise."
+                    }
                 ]
             }
         }
