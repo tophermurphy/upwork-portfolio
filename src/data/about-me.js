03479efcbf2a524ac21ebaa4bfd733843fa00310
tophermurphy <tophermurphy@gmail.com>
2024-12-17 16:20:39 -0600

    move stuff

---
+++ src/data/about-me.js
@@ -0,0 +1,27 @@
+export const aboutMe = {
+  title: "About Me",
+  id: "about-me",
+  slug: "about-me",
+  type: "about-me",
+  subtitle: "Chris Murphy, visual designer: UX, UI, & Front-End",
+  description:
+    "My long journey from artist to visual designer and front-end developer. While doing a bit of everything on the side",
+  content: {
+    quote: "It's hip to be square.",
+    quoteAuthor: "&ndash; Huey Lewis & the News",
+    body: [
+      'I was always a square peg in a round hole. My father, an engineer by trade, expected me to excel in math and I did, but eventually I became drawn to the arts and painting. I neither fit in with the intellectuals because I was too “artsy,” and I didn’t fit in with the art kids, because I was too "smart.”',
+      "I pursued my artistic passions studying painting at Rhode Island School of Design. There I incorporated my intellectual sensibilities into my artwork. My work encompassed urban landscapes as vast macro-organism. A constant attempt at reconciling opposites; the artificial with the organic, the rational with the intuitive.",
+      "It was difficult to find my niche as a painter, and I decided to start fresh in a studio apartment in Chicago. Efficiently utilizing the space I created a digital studio by balancing my computer on a drawing board propped on my couch-bed. There I hammered away, teaching myself design and applying what I learned by building my personal website.",
+      "As a designer, I am able to meld the intellectual with the artistic. Not satisfied with just the design component, I took it upon myself to learn how to code. Web design affords me the opportunity to build my designs and watch my creations come to life. I am always looking to harmonize my intuitive and rational natures in my life and work. Some see it as squaring the circle. <em>I see it as jamming a square peg into a round hole.</em>",
+    ],
+    images: [
+      {
+        filename: "chris-murphy-portrait.png",
+        alt: "A portrait of Chris Murphy, sipping a favorite stout at a local establishment.",
+      },
+    ],
+  },
+};
+
+export default aboutMe;
---
+++ src/data/h2o-cw.js
@@ -0,0 +1,250 @@
+export const h2oCw = {
+  title: "H2O Plus",
+  id: "h2o-cw",
+  slug: "h2o-plus-e-commerce-website",
+  type: "page",
+  subtitle: "e-commerce site",
+  description:
+    "Continuously evolving a brand to stay relevant and raising the quality to compete with prestige brands",
+  thumbnail: {
+    filename: "h2o-plus-website-thumbnail.jpg",
+  },
+  roles: ["Visual Design", "Front-End Development", "Photography"],
+  tools: ["Photoshop", "Html/Css", "Javascript/jQuery"],
+  platforms: ["MarketLive"],
+  headImages: [
+    {
+      filename: "serum-splash.jpg",
+      alt: "Serum bottle emerging from a rippling pool of water.",
+      widths: [524, 1048, 1572],
+      mobileWidth: "90vw",
+      defaultWidth: "524px",
+    },
+    {
+      filename: "shiny-products.jpg",
+      alt: "A trio of highly reflective skincare jars and tube.",
+      widths: [240, 278, 526, 804],
+      mobileWidth: "45vw",
+      defaultWidth: "278px",
+    },
+    {
+      filename: "shampoo-texture.jpg",
+      alt: "A creamy green shampoo texture.",
+      widths: [240, 365, 690, 1055],
+      mobileWidth: "45vw",
+      defaultWidth: "365px",
+    },
+  ],
+  content: {
+    section1: {
+      header: "Growing & Evolving with a Brand Online",
+      body: [
+        "For eight years as the in-house web designer at H2O Plus I grew along with the brand. As design trends, marketing techniques and technologies grew, as well as changes in the beauty market I grew and evolved with them, always raising bar and embracing new challenges every day.",
+        "The latest site visual update. The intention was to lighten the visual feel of the site. After the last rebranding the focus was on deep sea ingredients and water. This was too heavy for the water. Also the previous website template relied to much on keeping all content above the fold.",
+      ],
+      images: [
+        {
+          filename: "h20-plus-classic-homepage-0.jpg",
+          alt: "H2O Plus earliest homepage from the 90's",
+          widths: [245, 384, 768, 1152],
+          mobileWidth: "68vw",
+          defaultWidth: "384px",
+        },
+        {
+          filename: "h20-plus-classic-homepage-1.jpg",
+          alt: "H2O Plus classic homepage, with a lighter cleaner feel.",
+          widths: [218, 384, 768, 1152],
+          mobileWidth: "68vw",
+          defaultWidth: "384px",
+        },
+        {
+          filename: "h20-plus-classic-homepage-2.jpg",
+          alt: "H2O Plus transistional homepage with a deep undersea background.",
+          widths: [231, 360, 720, 1080],
+          mobileWidth: "64vw",
+          defaultWidth: "360px",
+        },
+        {
+          filename: "h20-plus-classic-homepage-3.jpg",
+          alt: "H2O Plus rebranding homepage with a boxless look and a deep water-like background.",
+          widths: [260, 390, 780, 1170],
+          mobileWidth: "72vw",
+          defaultWidth: "390px",
+        },
+        {
+          filename: "h20-plus-classic-homepage-4.jpg",
+          alt: "H2O Plus rebranding homepage cleaning up the previous homepage and creating a fresh clinical look.",
+          widths: [310, 462, 924, 1386],
+          mobileWidth: "86vw",
+          defaultWidth: "462px",
+        },
+      ],
+    },
+    section2: {
+      header: "Bigger, Brighter & Clearer",
+      body: [
+        "The homepage focused several large dynamic visuals, bold typography and quick copy to introduce the latest promotion. Although the current platform was not responsive, I was able to make the homepage adaptive, stretching the key visuals to feel the page maximizing the impage.",
+        "In addition to cleaning up the header, a larger footer was incorporated in two parts. The first to additional offers such as free shipping and more extensive site navigation at the bottom.",
+      ],
+      images: [
+        {
+          filename: "h2o-plus-homepage.jpg",
+          alt: "H2O Plus homepage. The fruition of a brand repositioning, for a clean, clinical look.",
+          widths: [360, 600, 1200, 1800],
+          mobileWidth: "100vw",
+          defaultWidth: "600px",
+        },
+      ],
+    },
+    section3: {
+      header: "A Clean, Bold, & Clinical Look was the Focus",
+      body: [
+        "Changing the background to white necessitated codifying a color scheme as well establishing a typographic system with the already chose typeface Trade Gothic Condensed.",
+        "Adding a light cerulean blue and soft greys allowed for the effect of a lighter cleaners water visual. <Light, Clean, Water, Science>. Partial boarders and complementing verticle and horizontal lines instead of full border helped define discrete spaces yet keep an open look.",
+      ],
+      images: [
+        {
+          filename: "h2o-plus-typography-headers.svg",
+          alt: "H2O Plus header typography - with Trade Gothic Condensed.",
+        },
+        {
+          filename: "h2o-plus-typography-paragraph.svg",
+          alt: "H2O Plus paragraph typography — with Trade Gothic Condensed.",
+        },
+        {
+          filename: "h20-plus-gateway.jpg",
+          alt: "H2O Plus gateway page featuring the different skin care collections.",
+          widths: [360, 600, 1200, 1800],
+          mobileWidth: "100vw",
+          defaultWidth: "360px",
+        },
+      ],
+    },
+    section4: {
+      header: "You Can Be Information Dense without Being Heavy",
+      body: [
+        "We did a complete overhaul of the product details page. The goal was to information dense, while  remaining easy to read and still feel light.",
+        "A well established hierarchy was necessary for a road map for the page layout. Vital concise info was present above the fold. Horizontal and vertical lines where used to define separate spaces while keeping everything open. And establishing a tight typographic scale for consistency and rhythm. ",
+      ],
+      images: [
+        {
+          filename: "h2o-plus-product-details.jpg",
+          alt: "H2O Plus product details page, featuring a product description, suggested products above the fold, key elements and full ingredients, videos, media appearances and reviews.",
+          widths: [360, 600, 1200, 1800],
+          mobileWidth: "100vw",
+          defaultWidth: "360px",
+        },
+      ],
+    },
+    section5: {
+      header: "Telling the Story of the Brand in Many Different Ways",
+      body: [
+        "There were many opportunities to tell the story of our brand with the use of landing pages throughout the site. This was always a great opportunity to explore unique design options.",
+      ],
+      images: [
+        {
+          filename: "h20-plus-landing-page-0.jpg",
+          alt: "H2O Plus landing page featuring the skin care collections.",
+          widths: [180, 300, 600, 900],
+          mobileWidth: "50vw",
+          defaultWidth: "300px",
+        },
+        {
+          filename: "h20-plus-landing-page-1.jpg",
+          alt: "H2O Plus landing page showing the transition to the rebranded packaging.",
+          widths: [188, 312, 624, 936],
+          mobileWidth: "52vw",
+          defaultWidth: "312px",
+        },
+        {
+          filename: "h20-plus-landing-page-2.jpg",
+          alt: "H2O Plus landing page featuring the company's many philanthropic endeavors.",
+          widths: [184, 306, 612, 918],
+          mobileWidth: "51vw",
+          defaultWidth: "306px",
+        },
+        {
+          filename: "h20-plus-landing-page-3.jpg",
+          alt: "H2O Plus landing page selling the benefits of the customer loyalty program.",
+          widths: [238, 396, 792, 1188],
+          mobileWidth: "66vw",
+          defaultWidth: "396px",
+        },
+      ],
+    },
+    section6: {
+      header: "Images Anchor Design & Are the Focal Point of Storytelling",
+      body: [
+        "The first big impact I was able to make on h2oplus.com was to create an implement a new stanadard for product photography. Consistent high quality product photography was primary in communicating that we were a prestige brand. I undertook rephotographing the entire collection to bring all products on the site up to this standard.",
+        "Over the years I was able to create colorful high impact story telling photos that anchored marketing promotions and brought the products to life in a new an fun way.",
+      ],
+      images: [
+        {
+          filename: "h2o-photography-product-silhouettes.jpg",
+          alt: "A collection of different product silhouette photos.",
+          widths: [360, 600, 1200, 1800],
+          mobileWidth: "100vw",
+          defaultWidth: "600px",
+        },
+        {
+          filename: "h2o-plus-photography-serum-splash.jpg",
+          alt: "A styled photo of serum in a rippling pool of water.",
+          widths: [159, 162, 324, 648],
+          mobileWidth: "44vw",
+          defaultWidth: "162px",
+        },
+        {
+          filename: "h2o-plus-photography-whitening.jpg",
+          alt: "A styled photo of whitening serum on a bright and bubbly background.",
+          widths: [202, 216, 432, 864],
+          mobileWidth: "56vw",
+          defaultWidth: "216px",
+        },
+        {
+          filename: "h2o-plus-photography-lip-candy.jpg",
+          alt: "A styled photo of lip balm on a background of candy hearts.",
+          widths: [195, 222, 444, 666],
+          mobileWidth: "54vw",
+          defaultWidth: "222px",
+        },
+        {
+          filename: "h2o-plus-photography-night-treatment.jpg",
+          alt: "A styled photo of a night treatment gel on a starry night background with rippling water.",
+          widths: [238, 260, 520, 780],
+          mobileWidth: "46vw",
+          defaultWidth: "260px",
+        },
+        {
+          filename: "h2o-plus-photography-holiday-sparkles.jpg",
+          alt: "A styled photo of a skin care collection on a background of festive red holiday sparkles.",
+          widths: [194, 209, 418, 627],
+          mobileWidth: "48vw",
+          defaultWidth: "209px",
+        },
+        {
+          filename: "h2o-plus-photography-bath-bubbles.jpg",
+          alt: "A styled photo of a pair of bath washes amidst floating bubbles.",
+          widths: [187, 201, 402, 804],
+          mobileWidth: "52vw",
+          defaultWidth: "201px",
+        },
+      ],
+    },
+    section7: {
+      body: [
+        "Over the years at H2O Plus I was able to continually create dymanic imagery, relevant and stunning web sites that remained relevant elevating the brand to compete with larger prestige brands. The site was recognized as MarketLive <em>Best Site on the Rise</em> in 2011. The quality of work helped gain venture capital and foreign investment.",
+      ],
+      images: [
+        {
+          filename: "h2o-plus-website-award.jpg",
+          alt: "H2O Plus award from MarketLive for 'Best Site on the Rise.'",
+          widths: [324, 336, 672, 1008],
+          mobileWidth: "100vw",
+          defaultWidth: "336px",
+        },
+      ],
+    },
+  },
+};
+
+export default h2oCw;
